6d853784ea74dfd250b023697ef3201908d336ee
Amichai Vaknin <amichaiv@codevalue.net>
2019-05-06 12:21:11 +0300

    create assignment to templates proj

---
+++ TemplatesManager/Assignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TemplatesManager
+{
+    class Assignment: TableEntity
+    {
+        public Guid Guid { get; set; }
+        public string OutcomeServiceUrl { get; set; }
+        public string ResultSourcedId { get; set; }
+        public string CustomContextMembershipsUrl { get; set; }
+        public string OAuthConsumerKey { get; set; }
+        public string ResourceLinkId { get; set; }
+        public string CourseName { get; set; }
+        public string LmsName { get; set; }
+        public string LtiName { get; set; }
+    }
+}
--- TemplatesManager/LandingPage.cs
+++ TemplatesManager/LandingPage.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
-using AssignmentsAccessor;
 using LtiLibrary.AspNetCore.Extensions;
 using LtiLibrary.NetCore.Lti.v1;
 using Microsoft.AspNetCore.Mvc;
@@ -25,12 +25,36 @@ namespace TemplatesManager
             log.LogInformation("New LMS Connection");
 
             LtiRequest ltiRequest = await req.ParseLtiRequestAsync();
-            var assignmentToDb = new Assignment(ltiRequest);
+            Assignment assignmentToDb = ParseToAssignment(ltiRequest);
             Assignment assignment = await GetOrAddToDbAsync(assignments, assignmentToDb);
 
             var urlWithParams = $"{RedirectUrl}/en?assignmentGuid={assignment.Guid}&userId={ltiRequest.UserId}";
             log.LogInformation($"Redirect to {urlWithParams}");
             return new RedirectResult(urlWithParams, true);
+        }
+
+        private static Assignment ParseToAssignment(LtiRequest ltiRequest)
+        {
+            var ltiRequestCustomParameters = ltiRequest.CustomParameters;
+            var customParams = ltiRequestCustomParameters.Split('&');
+            var membershipsUrlStatement = customParams.FirstOrDefault(param => param.Contains("custom_context_memberships_url"));
+            var membershipsValue = membershipsUrlStatement?.Split('=')[1];
+            
+            Assignment assignment = new Assignment
+            {
+                CourseName = ltiRequest.ContextTitle,
+                LmsName = ltiRequest.ToolConsumerInstanceName,
+                LtiName = ltiRequest.ResourceLinkTitle,
+                OutcomeServiceUrl = ltiRequest.LisOutcomeServiceUrl,
+                ResultSourcedId = ltiRequest.LisResultSourcedId,
+                CustomContextMembershipsUrl = membershipsValue,
+                OAuthConsumerKey = ltiRequest.ConsumerKey,
+                ResourceLinkId = ltiRequest.ResourceLinkId,
+                PartitionKey = $"{ltiRequest.ToolConsumerInstanceName}",
+                RowKey = $"{ltiRequest.ContextId}_{ltiRequest.ResourceLinkId}"
+            };
+
+            return assignment;
         }
 
         private static async Task<Assignment> GetOrAddToDbAsync(CloudTable assignments, Assignment assignment)
@@ -43,7 +67,7 @@ namespace TemplatesManager
                 return result;
             }
 
-            assignment.GenerateGuid();
+            assignment.Guid = Guid.NewGuid();
             
             TableOperation tableOperation = TableOperation.InsertOrMerge(assignment);
             await assignments.ExecuteAsync(tableOperation);
